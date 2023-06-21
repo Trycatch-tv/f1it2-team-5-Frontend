c6d11efaa4236f35be3b06e15192e66a9a73cfc7
Luis0251 <luis0251@hotmail.com>
2023-06-21 13:56:06 -0500

    feature:register added

--- frontend-app/src/pages/Register.jsx
+++ frontend-app/src/pages/Register.jsx
@@ -1,7 +1,42 @@
 
-
+import './Login.css'
 export const Register = () => {
   return (
-    <div>Register</div>
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Registrate</h3>
+          <div className="form-group mt-3">
+            <label>Nombre Completo</label>
+            <input
+              type="nombre"
+              className="form-control mt-1"
+              placeholder="Nombre"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Registrar
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
